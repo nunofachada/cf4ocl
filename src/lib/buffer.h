35be73a4af88906436abdc59aa8a4697b0589bbe
Nuno Fachada <nfachada@isr.ist.utl.pt>
2014-07-12 20:29:22 +0100

    Add buffer related function prototypes

--- src/lib/buffer.h
+++ src/lib/buffer.h
@@ -28,89 +28,54 @@
 #ifndef CL4_BUFFER_H
 #define CL4_BUFFER_H 
 
-#include <glib.h>
-#if defined(__APPLE__) || defined(__MACOSX)
-    #include <OpenCL/opencl.h>
-#else
-    #include <CL/opencl.h>
-#endif
-#include "wrapper.h"
+#include "memobj.h"
 
 /** @brief Buffer wrapper object. */
-typedef struct cl4_buffer CL4Buffer;
+typedef CL4MemObj CL4Buffer;
 
-/** @brief Create a ::CL4Event wrapper object by wrapping a given
- * OpenCL event. */
-CL4Event* cl4_event_new(cl_event event);
+/** @brief Create a ::CL4Buffer wrapper object. */
+CL4Buffer* cl4_buffer_new(CL4Context* ctx, cl_mem_flags flags,
+	size_t size, void *host_ptr, GError** err);
 
-/** @brief Decrements the reference count of the event wrapper 
- * object. If it reaches 0, the event wrapper object is 
- * destroyed. */
-void cl4_event_destroy(CL4Event* evt);
+CL4Event* cl4_buffer_read(CL4CQueue* cq, CL4Buffer* buf,
+	cl_bool blocking_read, size_t offset, size_t size, void *ptr,
+	CL4EventWaitList evt_wait_lst, GError** err);
 
-/**
- * @brief Get event information object.
- * 
- * @param evt The event wrapper object.
- * @param param_name Name of information/parameter to get.
- * @param err Return location for a GError, or NULL if error reporting
- * is to be ignored.
- * @return The requested information object. This object will be 
- * automatically freed when the wrapper object is destroyed. If an error 
- * occurs, NULL is returned.
- * */
-#define cl4_event_info(evt, param_name, err) \
-	cl4_wrapper_get_info((CL4Wrapper*) evt, NULL, param_name, \
-		(cl4_wrapper_info_fp) clGetEventInfo, CL_FALSE, err)
+CL4Event* cl4_buffer_write(CL4CQueue* cq, CL4Buffer* buf,
+	cl_bool blocking_write, size_t offset, size_t size, void *ptr,
+ 	CL4EventWaitList evt_wait_lst, GError** err);
 
-/** 
- * @brief Increase the reference count of the event object.
- * 
- * @param evt The event wrapper object. 
- * */
-#define cl4_event_ref(evt) \
-	cl4_wrapper_ref((CL4Wrapper*) evt)
+#ifdef CL_VERSION_1_1
 
-/**
- * @brief Alias to cl4_event_destroy().
- * 
- * @param evt Event wrapper object to destroy if reference count 
- * is 1, otherwise just decrement the reference count.
- * */
-#define cl4_event_unref(evt) cl4_event_destroy(evt)
+CL4Buffer* cl4_buffer_new_from_region(CL4Buffer* buf, 
+	cl_mem_flags flags, size_t origin, size_t size, GError** err);
 
-/**
- * @brief Get the OpenCL event object.
- * 
- * @param evt The event wrapper object.
- * @return The OpenCL event object.
- * */
-#define cl4_event_unwrap(evt) \
-	((cl_event) cl4_wrapper_unwrap((CL4Wrapper*) evt))		
+CL4Event* cl4_buffer_read_rect(CL4CQueue* cq, CL4Buffer* buf,
+	cl_bool blocking_read, const size_t* buffer_origin,
+	const size_t* host_origin, const size_t* region, 
+	size_t buffer_row_pitch, size_t buffer_slice_pitch, 
+	size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
+	CL4EventWaitList evt_wait_lst, GError** err);
 
-typedef GPtrArray* CL4EventWaitList;
+CL4Event* cl4_buffer_write_rect(CL4CQueue* cq, CL4Buffer* buf,
+	cl_bool blocking_write, const size_t* buffer_origin,
+	const size_t* host_origin, const size_t* region, 
+	size_t buffer_row_pitch, size_t buffer_slice_pitch, 
+	size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
+	CL4EventWaitList evt_wait_lst, GError** err);
 
-#define cl4_event_wait_list_new() \
-	g_ptr_array_new()
-	
-#define cl4_event_wait_list_add(event_wait_list, evt) \
-	g_ptr_array_add(event_wait_list, (void*) cl4_event_unwrap(evt))
+#endif
 
-#define cl4_event_wait_list_get_num_events(event_wait_list) \
-	((event_wait_list != NULL) ? event_wait_list->len : 0)
-	
-#define cl4_event_wait_list_get_clevents(event_wait_list) \
-	((event_wait_list != NULL) \
-		? (const cl_event*) event_wait_list->pdata \
-		: NULL)
-	
-#define cl4_event_wait_list_clear(event_wait_list) \
-	if (event_wait_list != NULL) \
-		g_ptr_array_remove_range( \
-			event_wait_list, 0, event_wait_list->len)
-	
-#define cl4_event_wait_list_destroy(event_wait_list) \
-	g_ptr_array_free(event_wait_list, TRUE)
+#ifdef CL_VERSION_1_2
+
+CL4Event* cl4_buffer_fill(CL4CQueue* cq, CL4Buffer* buf, 
+	const void *pattern, size_t pattern_size, size_t offset, 
+	size_t size, CL4EventWaitList evt_wait_lst, GError** err);
+
+#endif
+
+/// @todo We can emulate the OpenCL>1.0 functions if not available in
+/// OpenCL
 
 #endif
 
