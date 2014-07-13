8b13b6596be556ffc90ae6f06e40278712fca588
Nuno Fachada <nfachada@isr.ist.utl.pt>
2014-07-13 22:48:55 +0100

    Implement buffer methods (OCL1.0), compiling, not tested

--- src/lib/buffer.c
+++ src/lib/buffer.c
@@ -36,7 +36,32 @@ CL4Buffer* cl4_buffer_new(CL4Context* ctx, cl_mem_flags flags,
 	g_return_val_if_fail(ctx != NULL, NULL);
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-		
+	
+	cl_int ocl_status;
+	cl_mem buffer;
+	CL4Buffer* buf = NULL;
+	
+	buffer = clCreateBuffer(cl4_context_unwrap(ctx), flags, size,
+		host_ptr, &ocl_status);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to create buffer (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	buf = cl4_memobj_new_wrap(buffer);
+	
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
+
+finish:
+	
+	/* Return new buffer wrapper. */
+	return buf;
 
 }
 
@@ -51,6 +76,39 @@ CL4Event* cl4_buffer_read(CL4CQueue* cq, CL4Buffer* buf,
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
 
+	cl_int ocl_status;
+	cl_event event;
+	CL4Event* evt = NULL;
+	
+	ocl_status = clEnqueueReadBuffer(cl4_cqueue_unwrap(cq), 
+		cl4_memobj_unwrap(buf), blocking_read, offset, size, ptr,
+		cl4_event_wait_list_get_num_events(evt_wait_lst),
+		cl4_event_wait_list_get_clevents(evt_wait_lst), &event);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to read buffer (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	/* Wrap event and associate it with the respective command queue. 
+	 * The event object will be released automatically when the command
+	 * queue is released. */
+	evt = cl4_cqueue_produce_event(cq, event);
+		
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
+
+	/* An error occurred, return NULL to signal it. */
+	evt = NULL;
+	
+finish:
+	
+	/* Return event. */
+	return evt;
 
 }
 
@@ -65,7 +123,40 @@ CL4Event* cl4_buffer_write(CL4CQueue* cq, CL4Buffer* buf,
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
 
+	cl_int ocl_status;
+	cl_event event;
+	CL4Event* evt = NULL;
+	
+	ocl_status = clEnqueueWriteBuffer(cl4_cqueue_unwrap(cq), 
+		cl4_memobj_unwrap(buf), blocking_write, offset, size, ptr,
+		cl4_event_wait_list_get_num_events(evt_wait_lst),
+		cl4_event_wait_list_get_clevents(evt_wait_lst), &event);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to write buffer (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	/* Wrap event and associate it with the respective command queue. 
+	 * The event object will be released automatically when the command
+	 * queue is released. */
+	evt = cl4_cqueue_produce_event(cq, event);
+		
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
 
+	/* An error occurred, return NULL to signal it. */
+	evt = NULL;
+	
+finish:
+	
+	/* Return event. */
+	return evt;
+	
 }
 
 void* cl4_buffer_map(CL4CQueue* cq, CL4Buffer* buf,
@@ -80,6 +171,41 @@ void* cl4_buffer_map(CL4CQueue* cq, CL4Buffer* buf,
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
 
+	cl_int ocl_status;
+	cl_event event;
+	void* ptr = NULL;
+	
+	ptr = clEnqueueMapBuffer(cl4_cqueue_unwrap(cq), 
+		cl4_memobj_unwrap(buf), blocking_map, map_flags, offset, size,
+		cl4_event_wait_list_get_num_events(evt_wait_lst),
+		cl4_event_wait_list_get_clevents(evt_wait_lst), 
+		&event, &ocl_status);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to map buffer (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	/* Wrap event and associate it with the respective command queue. 
+	 * The event object will be released automatically when the command
+	 * queue is released. */
+	*evt = cl4_cqueue_produce_event(cq, event);
+		
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
+
+	/* An error occurred, return NULL to signal it. */
+	ptr = NULL;
+	*evt = NULL;
+	
+finish:
+	
+	/* Return host pointer. */
+	return ptr;
 
 }
 
@@ -96,6 +222,40 @@ CL4Event* cl4_buffer_copy(CL4CQueue* cq, CL4Buffer* src_buf,
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
 
+	cl_int ocl_status;
+	cl_event event;
+	CL4Event* evt = NULL;
+	
+	ocl_status = clEnqueueCopyBuffer(cl4_cqueue_unwrap(cq), 
+		cl4_memobj_unwrap(src_buf), cl4_memobj_unwrap(dst_buf),
+		src_offset, dst_offset, size,
+		cl4_event_wait_list_get_num_events(evt_wait_lst),
+		cl4_event_wait_list_get_clevents(evt_wait_lst), &event);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to write buffer (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	/* Wrap event and associate it with the respective command queue. 
+	 * The event object will be released automatically when the command
+	 * queue is released. */
+	evt = cl4_cqueue_produce_event(cq, event);
+		
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
+
+	/* An error occurred, return NULL to signal it. */
+	evt = NULL;
+	
+finish:
+	
+	/* Return event. */
+	return evt;
 
 }
 	
@@ -112,90 +272,124 @@ CL4Event* cl4_buffer_copy_to_image(CL4CQueue* cq, CL4Buffer* src_buf,
 	/* Make sure err is NULL or it is not set. */
 	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
 
+	cl_int ocl_status;
+	cl_event event;
+	CL4Event* evt = NULL;
+	
+	ocl_status = clEnqueueCopyBufferToImage(cl4_cqueue_unwrap(cq), 
+		cl4_memobj_unwrap(src_buf), cl4_memobj_unwrap(dst_img),
+		src_offset, dst_origin, region,
+		cl4_event_wait_list_get_num_events(evt_wait_lst),
+		cl4_event_wait_list_get_clevents(evt_wait_lst), &event);
+	gef_if_error_create_goto(*err, CL4_ERROR, 
+		CL_SUCCESS != ocl_status, CL4_ERROR_OCL, error_handler, 
+		"Function '%s': unable to copy buffer to image (OpenCL error %d: %s).",
+		__func__, ocl_status, cl4_err(ocl_status));
+	
+	/* Wrap event and associate it with the respective command queue. 
+	 * The event object will be released automatically when the command
+	 * queue is released. */
+	evt = cl4_cqueue_produce_event(cq, event);
+		
+	/* If we got here, everything is OK. */
+	g_assert (err == NULL || *err == NULL);
+	goto finish;
+	
+error_handler:
+	/* If we got here there was an error, verify that it is so. */
+	g_assert (err == NULL || *err != NULL);
+
+	/* An error occurred, return NULL to signal it. */
+	evt = NULL;
+	
+finish:
+	
+	/* Return event. */
+	return evt;
 
 }
 
-#ifdef CL_VERSION_1_1
-
-CL4Buffer* cl4_buffer_new_from_region(CL4Buffer* buf, 
-	cl_mem_flags flags, size_t origin, size_t size, GError** err) {
-
-	/* Make sure buf is not NULL. */
-	g_return_val_if_fail(buf != NULL, NULL);
-	/* Make sure err is NULL or it is not set. */
-	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-
-
-}
-
-CL4Event* cl4_buffer_read_rect(CL4CQueue* cq, CL4Buffer* buf,
-	cl_bool blocking_read, const size_t* buffer_origin,
-	const size_t* host_origin, const size_t* region, 
-	size_t buffer_row_pitch, size_t buffer_slice_pitch, 
-	size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
-	CL4EventWaitList evt_wait_lst, GError** err) {
-
-	/* Make sure cq is not NULL. */
-	g_return_val_if_fail(cq != NULL, NULL);
-	/* Make sure buf is not NULL. */
-	g_return_val_if_fail(buf != NULL, NULL);
-	/* Make sure err is NULL or it is not set. */
-	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-
-}
-
-CL4Event* cl4_buffer_write_rect(CL4CQueue* cq, CL4Buffer* buf,
-	cl_bool blocking_write, const size_t* buffer_origin,
-	const size_t* host_origin, const size_t* region, 
-	size_t buffer_row_pitch, size_t buffer_slice_pitch, 
-	size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
-	CL4EventWaitList evt_wait_lst, GError** err) {
-
-	/* Make sure cq is not NULL. */
-	g_return_val_if_fail(cq != NULL, NULL);
-	/* Make sure buf is not NULL. */
-	g_return_val_if_fail(buf != NULL, NULL);
-	/* Make sure err is NULL or it is not set. */
-	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-
-
-}
-
-CL4Event* cl4_buffer_copy_rect(CL4CQueue* cq, CL4Buffer* src_buf,
-	CL4Buffer* dst_buf, const size_t *src_origin, 
-	const size_t *dst_origin, const size_t *region, 
-	size_t src_row_pitch, size_t src_slice_pitch, size_t dst_row_pitch,
-	size_t dst_slice_pitch, CL4EventWaitList evt_wait_lst, 
-	GError** err) {
-
-	/* Make sure cq is not NULL. */
-	g_return_val_if_fail(cq != NULL, NULL);
-	/* Make sure src_buf is not NULL. */
-	g_return_val_if_fail(src_buf != NULL, NULL);
-	/* Make sure dst_buf is not NULL. */
-	g_return_val_if_fail(dst_buf != NULL, NULL);
-	/* Make sure err is NULL or it is not set. */
-	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-
-
-}
-
-#endif
-
-#ifdef CL_VERSION_1_2
-
-CL4Event* cl4_buffer_fill(CL4CQueue* cq, CL4Buffer* buf, 
-	const void *pattern, size_t pattern_size, size_t offset, 
-	size_t size, CL4EventWaitList evt_wait_lst, GError** err) {
-
-	/* Make sure cq is not NULL. */
-	g_return_val_if_fail(cq != NULL, NULL);
-	/* Make sure buf is not NULL. */
-	g_return_val_if_fail(buf != NULL, NULL);
-	/* Make sure err is NULL or it is not set. */
-	g_return_val_if_fail(err == NULL || *err == NULL, NULL);
-
-
-}
-
-#endif
+//~ #ifdef CL_VERSION_1_1
+//~ 
+//~ CL4Buffer* cl4_buffer_new_from_region(CL4Buffer* buf, 
+	//~ cl_mem_flags flags, size_t origin, size_t size, GError** err) {
+//~ 
+	//~ /* Make sure buf is not NULL. */
+	//~ g_return_val_if_fail(buf != NULL, NULL);
+	//~ /* Make sure err is NULL or it is not set. */
+	//~ g_return_val_if_fail(err == NULL || *err == NULL, NULL);
+//~ 
+//~ 
+//~ }
+//~ 
+//~ CL4Event* cl4_buffer_read_rect(CL4CQueue* cq, CL4Buffer* buf,
+	//~ cl_bool blocking_read, const size_t* buffer_origin,
+	//~ const size_t* host_origin, const size_t* region, 
+	//~ size_t buffer_row_pitch, size_t buffer_slice_pitch, 
+	//~ size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
+	//~ CL4EventWaitList evt_wait_lst, GError** err) {
+//~ 
+	//~ /* Make sure cq is not NULL. */
+	//~ g_return_val_if_fail(cq != NULL, NULL);
+	//~ /* Make sure buf is not NULL. */
+	//~ g_return_val_if_fail(buf != NULL, NULL);
+	//~ /* Make sure err is NULL or it is not set. */
+	//~ g_return_val_if_fail(err == NULL || *err == NULL, NULL);
+//~ 
+//~ }
+//~ 
+//~ CL4Event* cl4_buffer_write_rect(CL4CQueue* cq, CL4Buffer* buf,
+	//~ cl_bool blocking_write, const size_t* buffer_origin,
+	//~ const size_t* host_origin, const size_t* region, 
+	//~ size_t buffer_row_pitch, size_t buffer_slice_pitch, 
+	//~ size_t host_row_pitch, size_t host_slice_pitch, void *ptr,
+	//~ CL4EventWaitList evt_wait_lst, GError** err) {
+//~ 
+	//~ /* Make sure cq is not NULL. */
+	//~ g_return_val_if_fail(cq != NULL, NULL);
+	//~ /* Make sure buf is not NULL. */
+	//~ g_return_val_if_fail(buf != NULL, NULL);
+	//~ /* Make sure err is NULL or it is not set. */
+	//~ g_return_val_if_fail(err == NULL || *err == NULL, NULL);
+//~ 
+//~ 
+//~ }
+//~ 
+//~ CL4Event* cl4_buffer_copy_rect(CL4CQueue* cq, CL4Buffer* src_buf,
+	//~ CL4Buffer* dst_buf, const size_t *src_origin, 
+	//~ const size_t *dst_origin, const size_t *region, 
+	//~ size_t src_row_pitch, size_t src_slice_pitch, size_t dst_row_pitch,
+	//~ size_t dst_slice_pitch, CL4EventWaitList evt_wait_lst, 
+	//~ GError** err) {
+//~ 
+	//~ /* Make sure cq is not NULL. */
+	//~ g_return_val_if_fail(cq != NULL, NULL);
+	//~ /* Make sure src_buf is not NULL. */
+	//~ g_return_val_if_fail(src_buf != NULL, NULL);
+	//~ /* Make sure dst_buf is not NULL. */
+	//~ g_return_val_if_fail(dst_buf != NULL, NULL);
+	//~ /* Make sure err is NULL or it is not set. */
+	//~ g_return_val_if_fail(err == NULL || *err == NULL, NULL);
+//~ 
+//~ 
+//~ }
+//~ 
+//~ #endif
+//~ 
+//~ #ifdef CL_VERSION_1_2
+//~ 
+//~ CL4Event* cl4_buffer_fill(CL4CQueue* cq, CL4Buffer* buf, 
+	//~ const void *pattern, size_t pattern_size, size_t offset, 
+	//~ size_t size, CL4EventWaitList evt_wait_lst, GError** err) {
+//~ 
+	//~ /* Make sure cq is not NULL. */
+	//~ g_return_val_if_fail(cq != NULL, NULL);
+	//~ /* Make sure buf is not NULL. */
+	//~ g_return_val_if_fail(buf != NULL, NULL);
+	//~ /* Make sure err is NULL or it is not set. */
+	//~ g_return_val_if_fail(err == NULL || *err == NULL, NULL);
+//~ 
+//~ 
+//~ }
+//~ 
+//~ #endif
