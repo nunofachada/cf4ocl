8f66a5ac0669a5988bc691c92a7a1037bcbc4c4a
Nuno Fachada <nfachada@isr.ist.utl.pt>
2014-07-17 18:46:46 +0100

    Update profiler test for new lib approach/profiler refactor (wip)

--- src/tests/test_profiler_times.c
+++ src/tests/test_profiler_times.c
@@ -34,95 +34,143 @@
 static void timesTest() {
 	
 	/* Aux vars. */
-	guint numEvents = 5;
-	struct _cl_event ev1, ev2, ev3, ev4, ev5, ev6, ev7, ev8;
-	int status;
-	cl_command_queue queue1, queue2, queue3;
-	queue1 = clCreateCommandQueue(NULL, NULL, 0, NULL);
-	queue2 = clCreateCommandQueue(NULL, NULL, 0, NULL);
-	queue3 = clCreateCommandQueue(NULL, NULL, 0, NULL);
+	guint num_event_names = 5;
+	CL4Context* ctx;
+	CL4CQueue *q1, *q2, *q3;
+	CL4Event *ev1, *ev2, *ev3, *ev4, *ev5, *ev6, *ev7, *ev8;
+	CL4Buffer* buf;
+	CL4Program* prg;
+	CL4Kernel* krnl;
+	GError* err = NULL;
+	cl_int buf_host[100];
+	cl_event ev_unwrapped;
+	void* a_bug = NULL;
+	
+	/* Create OpenCL wrappers for testing. */
+	ctx = cl4_context_new_any(&err);
+	g_assert_no_error(err);
+	
+	dev = cl4_context_get_device(ctx, 0, &err);
+	g_assert_no_error(err);
+	
+	q1 = cl4_cqueue_new(ctx, dev, CL_QUEUE_PROFILING_ENABLE, &err);
+	g_assert_no_error(err);
+	q2 = cl4_cqueue_new(ctx, dev, CL_QUEUE_PROFILING_ENABLE, &err);
+	g_assert_no_error(err);
+	q3 = cl4_cqueue_new(ctx, dev, CL_QUEUE_PROFILING_ENABLE, &err);
+	g_assert_no_error(err);
+	
+	buf = cl4_buffer_new(ctx, CL_MEM_READ_ONLY, sizeof(cl_int) * 100, NULL, &err);
+	g_assert_no_error(err);
+	
+	prg = cl4_program_new_from_source(ctx, "__kernel void k1(_global int* a){}", &err);
+	g_assert_no_error(err);
+
+	krnl = cl4_program_get_kernel(prg, "k1", &err);
+	g_assert_no_error(err);
 	
 	/* Profiling object. */
 	CL4Prof* profile = cl4_prof_new();
-	g_assert(profile != NULL);
 
-	/* Test with 5 unique events */
-	ev1.start = 10;
-	ev1.end = 15;
-	ev1.queue = queue1;
-	status = cl4_prof_add(profile, "Event1", &ev1, NULL);
-	g_assert(status == CL4_SUCCESS);
+	/* Test with 5 different event names. */
+	ev1 = cl4_buffer_write(q1, buf, CL_TRUE, 0, sizeof(cl_int) * 100, a_bug, NULL, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev1, "Event1");
+	ev_unwrapped = cl4_event_unwrap(ev1);
+	ev_unwrapped.t_start = 10;
+	ev_unwrapped.t_end = 15;
 
-	ev2.start = 16;
-	ev2.end = 20;
-	ev2.queue = queue1;
-	status = cl4_prof_add(profile, "Event2", &ev2, NULL);
-	g_assert(status == CL4_SUCCESS);
+	cl4_buffer_map(q1, buf, CL_TRUE, CL_MAP_READ, 0, sizeof(cl_int) * 100, NULL, &ev2, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev2, "Event2");
+	ev_unwrapped = cl4_event_unwrap(ev2);
+	ev_unwrapped.t_start = 16;
+	ev_unwrapped.t_end = 20;
 
-	ev3.start = 17;
-	ev3.end = 30;
-	ev3.queue = queue2;
-	status = cl4_prof_add(profile, "Event3", &ev3, NULL);
-	g_assert(status == CL4_SUCCESS);
+	ev3 = cl4_memobj_unmap((CL4MemObj*) buf, q1, a_bug, NULL, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev3, "Event3");
+	ev_unwrapped = cl4_event_unwrap(ev3);
+	ev_unwrapped.t_start = 17;
+	ev_unwrapped.t_end = 30;
 
-	ev4.start = 19;
-	ev4.end = 25;
-	ev4.queue = queue3;
-	status = cl4_prof_add(profile, "Event4", &ev4, NULL);
-	g_assert(status == CL4_SUCCESS);
+	size_t gws = 256;
+	size_t lws = 16;
+	size_t gwo = 0;
 
-	ev5.start = 29;
-	ev5.end = 40;
-	ev5.queue = queue1;
-	status = cl4_prof_add(profile, "Event5", &ev5, NULL);
-	g_assert(status == CL4_SUCCESS);
+	ev4 = cl4_kernel_set_args_and_run(krnl, q3, 1, &gwo, &gws, &lws, NULL, &err, buf, NULL);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev4, "Event4");
+	ev_unwrapped = cl4_event_unwrap(ev4);
+	ev_unwrapped.t_start = 19;
+	ev_unwrapped.t_end = 25;
 
-	ev6.start = 35;
-	ev6.end = 45;
+	ev5 = cl4_buffer_read(q1, buf, CL_TRUE, 0, sizeof(cl_int) * 100, a_bug, NULL, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev5, "Event5");
+	ev_unwrapped = cl4_event_unwrap(ev5);
+	ev_unwrapped.t_start = 29;
+	ev_unwrapped.t_end = 40;
+
+	ev6 = cl4_buffer_write(q2, buf, CL_TRUE, 0, sizeof(cl_int) * 20, a_bug, NULL, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev6, "Event1");
+	ev_unwrapped = cl4_event_unwrap(ev6);
+	ev_unwrapped.t_start = 35;
+	ev_unwrapped.t_end = 45;
 	ev6.queue = queue2;
-	status = cl4_prof_add(profile, "Event1", &ev6, NULL);
-	g_assert(status == CL4_SUCCESS);
 
-	ev7.start = 68;
-	ev7.end = 69;
-	ev7.queue = queue1;
-	status = cl4_prof_add(profile, "Event1", &ev7, NULL);
-	g_assert(status == CL4_SUCCESS);
+	ev7 = cl4_kernel_set_args_and_run(krnl, q1, 1, &gwo, &gws, &lws, NULL, &err, buf, NULL);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev7, "Event1");
+	ev_unwrapped = cl4_event_unwrap(ev7);
+	ev_unwrapped.t_start = 68;
+	ev_unwrapped.t_end = 69;
 
-	ev8.start = 50;
-	ev8.end = 70;
-	ev8.queue = queue3;
-	status = cl4_prof_add(profile, "Event1", &ev8, NULL);
-	g_assert(status == CL4_SUCCESS);
+	ev8 = cl4_buffer_write(q3, buf, CL_TRUE, 0, sizeof(cl_int) * 20, a_bug, NULL, &err);
+	g_assert_no_error(err);
+	cl4_event_set_name(ev8, "Event1");
+	ev_unwrapped = cl4_event_unwrap(ev8);
+	ev_unwrapped.t_start = 50;
+	ev_unwrapped.t_end = 70;
 
-	status = cl4_prof_aggregate(profile, NULL);
-	g_assert(status == CL4_SUCCESS);
-	status = cl4_prof_overmat(profile, NULL);
-	g_assert(status == CL4_SUCCESS);
+	/* Add queues. */
+	cl4_prof_add_queue(prof, "Q1", q1);
+	cl4_prof_add_queue(prof, "Q2", q2);
+	cl4_prof_add_queue(prof, "Q3", q3);
+
+	/* Perform profiling calculations. */
+	cl4_prof_calc(prof, &err);
+	g_assert_no_error(err);
 	
 	/* ************************* */
 	/* Test aggregate statistics */
 	/* ************************* */
 	
-	CL4ProfEvAgg* agg;
+	CL4ProfEventAgg* agg;
 	
-	agg = (CL4ProfEvAgg*) g_hash_table_lookup(profile->aggregate, "Event1");
+	agg = cl4_prof_get_agg(profile, "Event1");
+	g_assert(agg != NULL);
 	g_assert_cmpuint(agg->totalTime, ==, 36);
 	g_assert_cmpfloat(agg->relativeTime - 0.51728, <, 0.0001);
 	
-	agg = (CL4ProfEvAgg*) g_hash_table_lookup(profile->aggregate, "Event2");
+	agg = cl4_prof_get_agg(profile, "Event2");
+	g_assert(agg != NULL);
 	g_assert_cmpuint(agg->totalTime, ==, 4);
 	g_assert_cmpfloat(agg->relativeTime - 0.05714, <, 0.0001);
 
-	agg = (CL4ProfEvAgg*) g_hash_table_lookup(profile->aggregate, "Event3");
+	agg = (CL4ProfEventAgg*) cl4_prof_get_agg(profile, "Event3");
+	g_assert(agg != NULL);
 	g_assert_cmpuint(agg->totalTime, ==, 13);
 	g_assert_cmpfloat(agg->relativeTime - 0.18571, <, 0.0001);
 
-	agg = (CL4ProfEvAgg*) g_hash_table_lookup(profile->aggregate, "Event4");
+	agg = (CL4ProfEventAgg*) cl4_prof_get_agg(profile, "Event4");
+	g_assert(agg != NULL);
 	g_assert_cmpuint(agg->totalTime, ==, 6);
 	g_assert_cmpfloat(agg->relativeTime - 0.08571, <, 0.0001);
 
-	agg = (CL4ProfEvAgg*) g_hash_table_lookup(profile->aggregate, "Event5");
+	agg = (CL4ProfEventAgg*) cl4_prof_get_agg(profile, "Event5");
+	g_assert(agg != NULL);
 	g_assert_cmpuint(agg->totalTime, ==, 11);
 	g_assert_cmpfloat(agg->relativeTime - 0.15714, <, 0.0001);
 
@@ -180,12 +228,8 @@ static void timesTest() {
 	g_free(name_used);
 	
 	/** @todo Change profcl so that it prints to a stream, then test  */
-	//cl4_prof_print_info(profile, CL4_PROF_AGGEVDATA_SORT_TIME);
+	//cl4_prof_print_info(profile, CL4_PROF_EVENTAGG_SORT_TIME);
 
-	/* Free queue. */
-	clReleaseCommandQueue(queue1);
-	clReleaseCommandQueue(queue2);
-	clReleaseCommandQueue(queue3);
 	
 	/* Free profile. */
 	cl4_prof_destroy(profile);
