8cf72883251e00cb9e38b3c04435847c4d61670d
FakenMC <faken@fakenmc.com>
2014-08-03 00:02:31 +0100

    OCL stub: update device function prototypes exactly as API
    Windows compilation didn't work because of the difference.

--- src/tests/ocl_stub/ocl_device.c
+++ src/tests/ocl_stub/ocl_device.c
@@ -27,9 +27,10 @@
 #include "ocl_env.h"
 #include "utils.h"
 
-cl_int clGetDeviceIDs(cl_platform_id platform, 
-	cl_device_type device_type, cl_uint num_entries, 
-	cl_device_id* devices, cl_uint* num_devices) {
+CL_API_ENTRY cl_int CL_API_CALL
+clGetDeviceIDs(cl_platform_id platform, cl_device_type device_type, 
+	cl_uint num_entries, cl_device_id* devices, cl_uint* num_devices) 
+	CL_API_SUFFIX__VERSION_1_0 {
 	
 	cl_int status = CL_SUCCESS;
 
@@ -59,9 +60,10 @@ cl_int clGetDeviceIDs(cl_platform_id platform,
 	return status;
 }
 
-cl_int clGetDeviceInfo(cl_device_id device, cl_device_info param_name, 
+CL_API_ENTRY cl_int CL_API_CALL
+clGetDeviceInfo(cl_device_id device, cl_device_info param_name, 
 	size_t param_value_size, void* param_value, 
-	size_t* param_value_size_ret) {
+	size_t* param_value_size_ret) CL_API_SUFFIX__VERSION_1_0 {
 		
 	cl_int status = CL_SUCCESS;
 
