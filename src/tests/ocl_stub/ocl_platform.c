d494028f5b37e0114f6415f8340e310aed13e121
FakenMC <faken@fakenmc.com>
2014-08-03 00:07:31 +0100

    OCL stub: update platform function prototypes exactly as API
    Windows compilation didn't work because of the difference.

--- src/tests/ocl_stub/ocl_platform.c
+++ src/tests/ocl_stub/ocl_platform.c
@@ -27,8 +27,9 @@
 #include "ocl_env.h"
 #include "utils.h"
 
-cl_int clGetPlatformIDs(cl_uint num_entries, cl_platform_id* platforms, 
-	cl_uint* num_platforms) {
+CL_API_ENTRY cl_int CL_API_CALL
+clGetPlatformIDs(cl_uint num_entries, cl_platform_id* platforms,
+	cl_uint* num_platforms) CL_API_SUFFIX__VERSION_1_0 {
 	
 	cl_int status = CL_SUCCESS;
 	
@@ -52,9 +53,10 @@ cl_int clGetPlatformIDs(cl_uint num_entries, cl_platform_id* platforms,
 	return status;
 }
 
-cl_int clGetPlatformInfo(cl_platform_id platform, 
-	cl_platform_info param_name, size_t param_value_size, 
-	void* param_value, size_t* param_value_size_ret) {
+CL_API_ENTRY cl_int CL_API_CALL
+clGetPlatformInfo(cl_platform_id platform, cl_platform_info param_name,
+	size_t param_value_size, void* param_value, 
+	size_t* param_value_size_ret) CL_API_SUFFIX__VERSION_1_0 {
 		
 	cl_int status = CL_SUCCESS;
 
