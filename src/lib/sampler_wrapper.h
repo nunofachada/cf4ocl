251ceb9af02018752ad6353552f0760c6ad86843
FakenMC <faken@fakenmc.com>
2014-08-14 19:58:54 +0100

    Sampler mod.: add info, ref, unref and unwrap macros

--- src/lib/sampler_wrapper.h
+++ src/lib/sampler_wrapper.h
@@ -67,6 +67,103 @@ CCLSampler* ccl_sampler_new(CCLContext* ctx, cl_bool normalized_coords,
 /* Create a new sampler wrapper object using a list of properties. */
 CCLSampler* ccl_sampler_new_full(CCLContext* ctx, 
 	const cl_sampler_properties *sampler_properties, GError** err);
+	
+/**
+ * Get a ::CCLWrapperInfo sampler information object.
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] smplr The sampler wrapper object.
+ * @param[in] param_name Name of information/parameter to get.
+ * @param[out] err Return location for a GError, or `NULL` if error
+ * reporting is to be ignored.
+ * @return The requested sampler information object. This object will
+ * be automatically freed when the sampler wrapper object is 
+ * destroyed. If an error occurs, NULL is returned.
+ * */
+#define ccl_sampler_get_info(smplr, param_name, err) \
+	ccl_wrapper_get_info((CCLWrapper*) smplr, NULL, param_name, \
+		(ccl_wrapper_info_fp) clGetSamplerInfo, CL_TRUE, err)
+
+/** 
+ * Macro which returns a scalar sampler information value. 
+ * 
+ * Use with care. In case an error occurs, zero is returned, which 
+ * might be ambiguous if zero is a valid return value. In this case, it
+ * is necessary to check the error object. 
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] smplr The sampler wrapper object.
+ * @param[in] param_name Name of information/parameter to get value of.
+ * @param[in] param_type Type of parameter (e.g. cl_uint, size_t, etc.).
+ * @param[out] err Return location for a GError, or `NULL` if error
+ * reporting is to be ignored.
+ * @return The requested sampler information value. This value will be 
+ * automatically freed when the sampler wrapper object is destroyed. 
+ * If an error occurs, zero is returned.
+ * */
+#define ccl_sampler_get_scalar_info(smplr, param_name, param_type, err) \
+	*((param_type*) ccl_wrapper_get_info_value((CCLWrapper*) smplr, \
+		NULL, param_name, (ccl_wrapper_info_fp) clGetSamplerInfo, \
+		CL_TRUE, err))
+
+/** 
+ * Macro which returns an array sampler information value. 
+ * 
+ * @note As of OpenCL 2.0, there are no array sampler information
+ * values. This macro is provided for completeness.
+ * 
+ * Use with care. In case an error occurs, NULL is returned, which 
+ * might be ambiguous if NULL is a valid return value. In this case, it
+ * is necessary to check the error object. 
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] smplr The sampler wrapper object.
+ * @param[in] param_name Name of information/parameter to get value of.
+ * @param[in] param_type Type of parameter (e.g. char*, size_t*, etc.).
+ * @param[out] err Return location for a GError, or `NULL` if error
+ * reporting is to be ignored.
+ * @return The requested sampler information value. This value will be 
+ * automatically freed when the sampler wrapper object is destroyed. 
+ * If an error occurs, NULL is returned.
+ * */
+#define ccl_sampler_get_array_info(smplr, param_name, param_type, err) \
+	(param_type) ccl_wrapper_get_info_value((CCLWrapper*) smplr, \
+		NULL, param_name, (ccl_wrapper_info_fp) clGetSamplerInfo, \
+		CL_TRUE, err)
+
+/** 
+ * Increase the reference count of the sampler wrapper object.
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] sampler The sampler wrapper object. 
+ * */
+#define ccl_sampler_ref(smplr) \
+	ccl_wrapper_ref((CCLWrapper*) smplr)
+
+/**
+ * Alias to ccl_sampler_destroy().
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] smplr sampler wrapper object to destroy if reference count
+ * is 1, otherwise just decrement the reference count.
+ * */
+#define ccl_sampler_unref(smplr) ccl_sampler_destroy(smplr)
+
+/**
+ * Get the OpenCL sampler object.
+ * 
+ * @public @memberof ccl_sampler
+ * 
+ * @param[in] smplr The sampler wrapper object.
+ * @return The OpenCL sampler object.
+ * */
+#define ccl_sampler_unwrap(smplr) \
+	((cl_sampler) ccl_wrapper_unwrap((CCLWrapper*) smplr))	
 
 /** @} */
 
