41b921d94d8bdb15592678455103088b166f3637
FakenMC <faken@fakenmc.com>
2014-08-16 00:52:39 +0100

    Add several doc scripts + doc improvements
    - Add Git SHA1 to project semantic versioning in docs
    - Add Git SHA1 to file version in docs
    - Add scripts to automatically link to OpenCL functions API URL
    when an OpenCL function is mentioned in the docs
    - Make previous feature optional, because it's slow and may not
    work on all platforms
    - Set version to 2.0.0-alpha.1 (2.0.0-alpha.1+gitsha1 in docs)

---
+++ docscripts/parseoclfuns.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# This script scrapes the OpenCL function names from the OpenCL headers and obtains 
+# the most recent documentation URLs for each function.
+#
+# Requires: ctags curl cut head
+# Author: Nuno Fachada <faken@fakenmc.com>
+# Licence: GNU General Public License version 3 (GPLv3)
+# Date: 2014
+#
+macros_to_ignore=CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED,CL_EXT_SUFFIX__VERSION_1_2_DEPRECATED,CL_EXT_SUFFIX__VERSION_2_0_DEPRECATED,CL_API_SUFFIX__VERSION_1_0,CL_API_SUFFIX__VERSION_1_1,CL_API_SUFFIX__VERSION_1_2,CL_API_SUFFIX__VERSION_2_0,CL_API_ENTRY,CL_API_CALL
+ocl_versions="2.0 1.2 1.1 1.0"
+ocl_functions=`LC_ALL=C ctags -I $macros_to_ignore -x --c-kinds=p ../ocl/2.0/CL/* | cut -f1 -d " "`
+for ocl_fun in $ocl_functions
+do
+  for ocl_ver in $ocl_versions
+  do
+    doc_url="http://www.khronos.org/registry/cl/sdk/${ocl_ver}/docs/man/xhtml/${ocl_fun}.html"
+    doc_status=`curl -s --head ${doc_url} | head -n 1 | cut -f2 -d " "`
+    if [[ $doc_status == "200" ]]
+    then
+      echo "${ocl_fun}() ${doc_url}"
+      break
+    fi
+  done
+done
