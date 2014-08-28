da2494be8714b8916002d7f2de8d2e72aa70555d
FakenMC <faken@fakenmc.com>
2014-08-28 14:57:34 +0100

    Add support for C++

--- src/cf4ocl2.h
+++ src/cf4ocl2.h
@@ -1,25 +1,25 @@
-/*   
+/*
  * This file is part of cf4ocl (C Framework for OpenCL).
- * 
+ *
  * cf4ocl is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
- * cf4ocl is distributed in the hope that it will be useful, 
+ *
+ * cf4ocl is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with cf4ocl. If not, see 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with cf4ocl. If not, see
  * <http://www.gnu.org/licenses/>.
  * */
- 
-/** 
+
+/**
  * @file
  * Aggregate header for _cf4ocl_.
- * 
+ *
  * @author Nuno Fachada
  * @date 2014
  * @copyright [GNU Lesser General Public License version 3 (LGPLv3)](http://www.gnu.org/licenses/lgpl.html)
@@ -28,10 +28,14 @@
 #ifndef _CCL_CF4OCL2_H_
 #define _CCL_CF4OCL2_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <cf4ocl2/abstract_dev_container_wrapper.h>
 #include <cf4ocl2/abstract_wrapper.h>
 #include <cf4ocl2/buffer_wrapper.h>
-#include <cf4ocl2/common.h> 
+#include <cf4ocl2/common.h>
 #include <cf4ocl2/context_wrapper.h>
 #include <cf4ocl2/device_query.h>
 #include <cf4ocl2/device_selector.h>
@@ -50,5 +54,9 @@
 #include <cf4ocl2/queue_wrapper.h>
 #include <cf4ocl2/sampler_wrapper.h>
 #include <cf4ocl2/types.h>
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
