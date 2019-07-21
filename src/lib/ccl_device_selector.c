95e8ae6ca9edafdb593776c03e48655b2ea8d05a
FakenMC <faken@fakenmc.com>
2019-07-21 20:36:36 +0100

    Fix bug in ccl_devsel_indep_type() independent filter
    Was returning true when an error occured (e.g. incorrect data)

--- src/lib/ccl_device_selector.c
+++ src/lib/ccl_device_selector.c
@@ -699,9 +699,9 @@ cl_bool ccl_devsel_indep_type(CCLDevice * dev, void * data, CCLErr ** err) {
     CCLErr * err_internal = NULL;
 
     /* Device type. */
-    cl_device_type type = CL_DEVICE_TYPE_DEFAULT;
+    cl_device_type type = 0;
     /* Device type to check for. */
-    cl_device_type type_to_check = CL_DEVICE_TYPE_DEFAULT;
+    cl_device_type type_to_check = 0;
 
     /* Make sure data is not NULL. */
     g_if_err_create_goto(*err, CCL_ERROR, data == NULL,
